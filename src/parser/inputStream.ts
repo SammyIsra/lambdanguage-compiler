64f97c58f60d634f468877fd998772e61a0cb77c
SammyIsra <sammy.israwi@gmail.com>
2019-06-13 19:43:05 -0400

    Built the input stream section fo the parser

---
+++ src/parser/inputStream.ts
@@ -0,0 +1,38 @@
+/** Creates an input stream that provides methods to read the input string */
+export default function createInputStream(input: string) {
+  /** Current position on the stream */
+  let position = 0;
+  /** Current line */
+  let line = 1;
+  /** Current column */
+  let column = 0;
+
+  return {
+    /** Returns the next character on the input string */
+    next() {
+      const char = input[position++];
+      if (char === "\n") {
+        line++;
+        column = 0;
+      } else {
+        column++;
+      }
+      return char;
+    },
+
+    /** Shows the next character on the input string */
+    peek() {
+      return input[position];
+    },
+
+    /** Determines if we are at the end of the file */
+    eof() {
+      this.peek() === undefined;
+    },
+
+    /** Throws an error */
+    croak(msg: string = "Unknown Error") {
+      throw new Error(`Input Stream Error | ${msg}, Ln ${line}, Col ${column}`);
+    }
+  };
+}
